00d1da2f4aad39b0f6d83726cb59afc10e068bba
realvineeths <vineeth14shivu@gmail.com>
2022-07-14 23:52:59 +0530

    deploy my react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      userarr:[]
+    }
+  }
+  componentDidMount(){
+
+    fetch("https://demo1779595.mockable.io/companies",{
+    method:"POST"
+  })
+    .then(res=>res.json())
+    .then(data=>{
+      // const datacomp=data.companiesList;
+      // const randomname=datacomp[Math.floor(Math.random()*datacomp.length)];
+      // console.log(data.companiesList[0].name);}
+      this.setState({userarr:data.companiesList})
+      console.log(this.state.userarr)
+    }
+  )
+
+}
+  render() {
+    return(
+      <div>
+        <table>
+          <thead id='head'>
+            <tr>
+              <th>name</th>
+              <th>company</th>
+              <th>role</th>
+              {/* <th>verified</th> */}
+              <th>status</th>
+              <th>companyImageURL</th>
+            </tr>
+          </thead>
+          <tbody id='body'>
+            {
+              this.state.userarr.map(user=>
+                <tr>
+                  <td>{user.name}</td>
+                  <td>{user.company}</td>
+                  <td>{user.role}</td>
+                  {/* <td>{user.verified}</td> */}
+                  <td>{user.status}</td>
+                  <td>{user.companyImageURL}</td>
+                </tr>
+              )
+            }         
+          </tbody>
+        </table>
+      </div>
+    );
+
+    
+
+  }
 }
 
-export default App;
+export default App
